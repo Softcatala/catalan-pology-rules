4bfb81cf613afaf2bebcfc95682177600ce8f7c4
Jordi Mas <jmas@softcatala.org>
2015-06-15 20:21:19 +0200

    First version

---
+++ check.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+get_number_of_messages_in_po_file() 
+{
+    msgfmt $1 --statistics 2> results.txt 
+    sed 's/[^0-9]//g' results.txt > strings.txt
+    strings=`cat strings.txt`
+    echo $strings
+}
+
+# Look for the error message with the exact number of errors
+# what pology returned
+get_pology_error_msg_for_rules_errors()
+{
+    # Get number of strings in PO file
+    msgfmt $1 --statistics 2> results.txt 
+    sed 's/[^0-9]//g' results.txt > strings.txt
+    strings=`cat strings.txt`
+    error="===== Les regles han detectat " 
+    error+=$(get_number_of_messages_in_po_file $1)
+    error+=" problem"
+    echo $error
+}
+
+for filename in *.rules; do
+
+    filename="${filename%.*}"
+    correct="$filename-correct.po"
+    incorrect="$filename-incorrect.po"
+
+    # Check correct sentences for the rule
+    posieve check-rules -s rfile:date-format.rules $correct > results.txt
+
+    if grep -q "===== Les regles han detectat" results.txt; then
+        echo "Pology found error(s) in file" $correct "where no errors were expected" 
+    fi
+
+    # Check incorrect sentences
+    error=$(get_pology_error_msg_for_rules_errors "$incorrect")
+
+    # Check wrong sentences for the rule
+    posieve check-rules -s rfile:date-format.rules $incorrect > results.txt
+
+    if ! grep -q "$error" results.txt; then
+        echo "Pology found different number of error(s) in file" $incorrect "than expected" 
+    fi
+done
