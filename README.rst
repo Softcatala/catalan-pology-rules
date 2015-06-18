51191a1cbf66e96e44cd2996531b791e45bf8a08
Jordi Mas <jmas@softcatala.org>
2015-06-18 08:27:50 +0200

    Initial terminology file

--- README.rst
+++ README.rst
@@ -1,14 +1,24 @@
-.. image:: https://travis-ci.org/jordimas/catalan-pology-rules.svg
+.. image:: https://travis-ci.org/Softcatala/catalan-pology-rules.svg
     :target: https://travis-ci.org/Softcatala/catalan-pology-rules
 
-
-============
 Introduction
 ============
+
+This repository collects all the Pology rules developed by Softcatalà.
+
+This repository is intended to facilitate the development of new rules.
+
+Once the new rules are completed they will be contributed to upstream (Pology project).
+
+Continuous integration experiment
+=================================
 
 For every rule file there is a:
 
 * rule-file-correct.po -> when pology runs the rule against this file no errors are expected
 * rule-file-incorrect.po -> when pology runs the rule against this file all the strings are expected to return error
 
+Contact Information
+===================
 
+Jordi Mas: jmas@softcatala.org
--- check.sh
+++ check.sh
@@ -10,28 +10,13 @@ get_number_of_messages_in_po_file()
 
 # Look for the error message with the exact number of errors
 # what pology returned in Catalan
-get_pology_error_catalan_msg_for_rules_errors()
+get_pology_error_msg_for_rules_errors()
 {
     # Get number of strings in PO file
     msgfmt $1 --statistics 2> results.txt 
     sed 's/[^0-9]//g' results.txt > strings.txt
     strings=`cat strings.txt`
-    error="===== Les regles han detectat " 
-    error+=$(get_number_of_messages_in_po_file $1)
-    error+=" problem"
-    echo $error
-}
-
-
-# Look for the error message with the exact number of errors
-# what pology returned in English
-get_pology_error_english_msg_for_rules_errors()
-{
-    # Get number of strings in PO file
-    msgfmt $1 --statistics 2> results.txt 
-    sed 's/[^0-9]//g' results.txt > strings.txt
-    strings=`cat strings.txt`
-    error="===== Rules detected " 
+    error=$2
     error+=$(get_number_of_messages_in_po_file $1)
     error+=" problem"
     echo $error
@@ -45,25 +30,32 @@ for filename in *.rules; do
     incorrect="$filename-incorrect.po"
 
     # Check correct sentences for the rule
-    posieve check-rules -s rfile:date-format.rules $correct > results.txt
+    if [ -s "$correct" ]; then
+        posieve check-rules -s rfile:date-format.rules $correct > results.txt
 
-    if grep -q "===== Les regles han detectat" results.txt; then
-        echo "Pology found error(s) in file" $correct "where no errors were expected"
-        failed=1
+        if grep -q "===== Les regles han detectat" results.txt; then
+            echo "Pology found error(s) in file" $correct "where no errors were expected"
+            cat results.txt
+            failed=1
+        fi
     fi
 
     # Check incorrect sentences
-    error_ca=$(get_pology_error_catalan_msg_for_rules_errors "$incorrect")
-    error_en=$(get_pology_error_english_msg_for_rules_errors "$incorrect")
-    posieve check-rules -s rfile:date-format.rules $incorrect > results.txt
+    if [ -s  "$incorrect" ]; then
+        error_ca=$(get_pology_error_msg_for_rules_errors "$incorrect" "===== Les regles han detectat ")
+        error_en=$(get_pology_error_msg_for_rules_errors "$incorrect" "===== Rules detected ")
+        posieve check-rules -s rfile:date-format.rules $incorrect > results.txt
 
-    if ! grep -q "$error_ca" results.txt && ! grep -q "$error_en" results.txt ; then
-        echo "Pology found different number of error(s) in file" $incorrect "than expected"
-        cat results.txt
-        echo $error
-        failed=1
+        if ! grep -q "$error_ca" results.txt && ! grep -q "$error_en" results.txt ; then
+            echo "Pology found different number of error(s) in file" $incorrect "than expected"
+            cat results.txt
+            echo $error
+            failed=1
+        fi
     fi
 done
+
+rm results.txt
 
 if [ $failed -eq 1 ]; then
     exit 1
