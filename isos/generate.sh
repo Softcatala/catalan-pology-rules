09d08aa24ae9ea6cee1951e35e9c0d4a7acb8819
Jordi Mas <jmas@softcatala.org>
2015-06-24 20:20:49 +0200

    Add 3166

--- isos/generate.sh
+++ isos/generate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ./po-to-rules.py -i iso_639-3.57.ca.po -t iso_639.mustache -e iso_639-exceptions.json -o ../iso_639.rules
+./po-to-rules.py -i iso_3166-3.57.ca.po -t iso_3166.mustache -o ../iso_3166.rules
